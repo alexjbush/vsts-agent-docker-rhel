6f1716acf2f94f85cc9d654aa1ead3b4370b9ab6
Alex Bush <alex.bush@coxauto.co.uk>
2017-03-29 16:37:41 +0100

    added .net in standard and oracle jdk build

--- build.py
+++ build.py
@@ -7,7 +7,8 @@ configs={ 'CENTOS_VERSION': '7.2',
           'DOCKER_REPO': 'bushnoh/vsts-agent-docker-rhel',
           'DOCKER_VERSION': '1.13.1',
           'DOCKER_SHA256': '97892375e756fd29a304bd8cd9ffb256c2e7c8fd759e12a55a6336e15100ad75',
-          'DOCKER_COMPOSE_VERSION': '1.11.2' } 
+          'DOCKER_COMPOSE_VERSION': '1.11.2' }
+oracle_url_map={ '1.8.0_60': 'http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm' }
 
 def parse_template_string(input_string,configs):
     pattern = re.compile('%%([0-9A-Z_]+)%%')
@@ -44,6 +45,12 @@ def main():
     template_file('{}.{}'.format(base_file,'template'),
                   '{}/{}'.format(BUILD_DIR,base_file),
                   dict(configs,BASE_IMAGE_TAG='centos-7.2-docker-1.13.1',OPENJDK_VERSION='1.8.0')) 
+    base_file='centos/%%CENTOS_VERSION%%/docker/%%DOCKER_VERSION%%/standard/oraclejdk/%%ORACLE_JDK_VERSION%%/Dockerfile'
+    oracle_vers='1.8.0_60'
+    template_file('{}.{}'.format(base_file,'template'),
+                  '{}/{}'.format(BUILD_DIR,base_file),
+                  dict(configs,BASE_IMAGE_TAG='centos-7.2-docker-1.13.1-standard',
+                       ORACLE_JDK_VERSION=oracle_vers, ORACLE_JDK_REPO_URL=oracle_url_map['1.8.0_60'])) 
 
 if __name__ == "__main__":
     main()
